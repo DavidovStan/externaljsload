e3c095428a56468aaee89a470d57f82ca901896a
DavidovStan <78732688+DavidovStan@users.noreply.github.com>
2021-02-08 13:17:44 +0200

    Add files via upload

---
+++ header.js
@@ -0,0 +1,12 @@
+const Header = () => {
+    return (
+        <header>
+            <a class="item"> Item 1</a>
+            <a class="item active">Item 2</a>
+            <a class="item">Item 3</a>
+            <Button size='small' color='green'>
+                <Icon name='download' />download
+            </Button>
+        </header>
+    )
+}
