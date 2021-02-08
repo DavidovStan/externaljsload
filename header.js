9bde5af131cc5f9316163441a12140b4fbad5d0c
DavidovStan <78732688+DavidovStan@users.noreply.github.com>
2021-02-08 13:49:28 +0200

    Update header.js

--- header.js
+++ header.js
@@ -4,9 +4,6 @@ const Header = () => {
             <a class="item"> Item 1</a>
             <a class="item active">Item 2</a>
             <a class="item">Item 3</a>
-            <Button size='small' color='green'>
-                <Icon name='download' />download
-            </Button>
         </header>
     )
 }
