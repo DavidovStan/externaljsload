a5b3f47f309e8b822f884715286e60fa77b9d6f7
DavidovStan <78732688+DavidovStan@users.noreply.github.com>
2021-02-09 09:23:50 +0200

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,28 +1,7 @@
 class Hello extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      data: ''
-    }
-  }
-  getData(){
-      const script = document.createElement("script");
-      script.src = 'https://raw.githubusercontent.com/DavidovStan/externaljsload/main/hello.js'
-      script.async = true;
-      document.body.appendChild(script);
-  
-      this.setState({
-        data: script.src
-      })
-  }
-  componentDidMount () {
-      this.getData();
-  }
   render () {
     return (
-      <div>{this.state.data}</div>
+      <div>TEST IMPORT REACT 01</div>
     )
   }
-
-  
 }
